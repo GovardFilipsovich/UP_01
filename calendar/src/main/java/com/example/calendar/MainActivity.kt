55aec02061c53ce00f7fd328d68b54e23f18c032
user41 <user01@user.41>
2023-11-11 14:22:09 +0300

    Сделано 3 задание уведомлений

--- calendar/src/main/java/com/example/calendar/MainActivity.kt
+++ calendar/src/main/java/com/example/calendar/MainActivity.kt
@@ -1,14 +1,61 @@
 package com.example.calendar
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CalendarView
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import java.time.LocalDate
 import java.util.Date
 
-class MainActivity : AppCompatActivity() {
-    private var date = LocalDate.of(2003,9, 18)
+class MainActivity : AppCompatActivity(), CalendarView.OnDateChangeListener {
+    private lateinit var manager: NotificationManagerCompat
+    private var people = mapOf<String, LocalDate>(
+        Pair("Василий Панков", LocalDate.of(2003,11, 5)),
+        Pair("Кирилл Гайкин", LocalDate.of(2003,9, 18)),
+        Pair("Игорь Осыкин", LocalDate.of(2003,11, 11)),
+        )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var cal = findViewById<CalendarView>(R.id.cal)
+        cal.setOnDateChangeListener(this)
+        manager = NotificationManagerCompat.from(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = resources.getString(R.string.NOT_IMPORTANT_CHANNEL_NAME)
+            val chanel = NotificationChannel("NORMAL_CHANEL", name, NotificationManager.IMPORTANCE_LOW)
+            chanel.description = resources.getString(R.string.NOT_IMPORTANT_CHANNEL_DESCRIPTION)
+            chanel.enableVibration(false)
+            manager.createNotificationChannel(chanel)
+        }
     }
+
+    override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
+
+
+
+            people.forEach{ human ->
+                var date = human.value
+                var name = human.key
+                if (date.month.ordinal == p2  && date.dayOfMonth == p3){
+                    var builder = NotificationCompat.Builder(this, "NORMAL_CHANEL")
+                    builder.setSmallIcon(android.R.drawable.sym_def_app_icon)
+                        .setContentTitle("Не забудьте поздравить!!!")
+                        .setContentText("Сегодня день рождение у " + name)
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) { return }
+
+                    manager.notify(R.id.BOLSHOI_NOTFICATION_ID, builder.build())
+            }
+
+        }
+    }
+
+
 }
